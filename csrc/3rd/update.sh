e20a50229002a0c97bb72de32c3dac4c03228c4f
jssonx <yx546@cornell.edu>
2024-03-17 01:16:13 +0000

    feat: add grouped gemm v0

--- csrc/3rd/update.sh
+++ csrc/3rd/update.sh
@@ -6,7 +6,10 @@ git clone https://github.com/NVIDIA/cutlass.git --depth=1 cutlass.origin
 
 rm -rf cutlass
 mkdir -p cutlass
+mkdir -p cutlass/tools/util/include/cutlass
+
 cp -r cutlass.origin/include cutlass
+cp -r cutlass.origin/tools/util/include/cutlass/util cutlass/tools/util/include/cutlass
 
 git -C cutlass.origin/ rev-parse HEAD > cutlass/readme
 
